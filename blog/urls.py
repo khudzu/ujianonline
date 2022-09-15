23ee5b1bb3350ced188a03bdcede646625ea8cbb
khudzu <64119084+khudzu@users.noreply.github.com>
2022-09-15 23:39:51 +0700

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,4 @@ app_name='blog'
 urlpatterns = [
 	re_path(r'^create/$', views.create, name='create'),
 	re_path(r'^$', views.index, name='index'),
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
