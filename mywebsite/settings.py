122dfa03f8ae316e6ff0c17ea2de15cca49b5c73
khudzu <64119084+khudzu@users.noreply.github.com>
2022-10-31 15:32:30 +0700

    Update settings.py

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -118,8 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # Configure Django App for Heroku.
 import django_heroku
 django_heroku.settings(locals())
